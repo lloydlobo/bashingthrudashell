69cd854284c2401d0f447f07ed133e40d779d9f5
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-14 19:47:50 +0530

    feat: add practice script for install.sh
    - learning to install packages on linux installs

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# “Use the interpreter located at usr/bin/env to run the above script, with bash.”
+
+# using the above as direct reference to location of the shell on target machine
+# #!/bin/bash
+
+# Inform script status & shell info
+echo Running in $SHELL 
+
+# Install terminal stuff
+# sudo pacman -S kitty vim vi
+echo "Hi"
+
+
