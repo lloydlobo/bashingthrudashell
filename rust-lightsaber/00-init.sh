46b6f9a2697401ca5837bbf128c68b4188cfd751
lloydlobo <76430758+lloydlobo@users.noreply.github.com>
2022-08-29 15:46:53 +0530

    feat(rust): init 00-init.sh install plg

---
+++ rust-lightsaber/00-init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo dnf copr enable atim/lazygit -y
+sudo dnf install -y neovim git ripgrep fzf lazygit ncdu htop btop python nodejs
+
+# https://github.com/praxeo/rust-lightsaber/blob/master/00-init.sh
